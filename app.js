b8e3931894ef09d4f6d29a864974eebcc2eda30f
Ubuntu <ubuntu@ip-172-31-1-204.ap-south-1.compute.internal>
2025-07-10 13:05:12 +0000

    Initial Node.js DevOps app

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('🚀 Hello from Satendra’s Node.js DevOps App!'));
+app.listen(port, () => console.log(`App running on port ${port}`));
